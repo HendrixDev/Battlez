93f0a053918a5f754ae1caa8ba790bf779a7232e
isaiah hendrix <isaiah.hendrix1@outlook.com>
2025-11-04 20:38:44 -0600

    created a second monster for PlayerOne and created a PlayerTwo object with one monster

--- Battlez/Program.cs
+++ Battlez/Program.cs
@@ -22,9 +22,23 @@ playerOne.Monsters.Add(monsterOne);
 
 //TODO: create a second monster and add it to playerOne's Monsters list (be sure to assign values to all properties)
 
+Monster MonsterTwo = new Monster();
+MonsterTwo.Name = "charmander";
+MonsterTwo.Health = 80;
+MonsterTwo.Type = Element.Fire;
 
+playerOne.Monsters.Add(MonsterTwo);
 
 
 //TODO: create new player object called playerTwo and assign values to its properties (be sure to add at least one monster to playerTwo's Monsters list)
 
+Player PlayerTwo = new Player();
 
+PlayerTwo.Name = "gary";
+PlayerTwo.Monsters = new List<Monster>();
+Monster MonsterOne = new Monster();
+MonsterOne.Name = "squirtle";
+MonsterOne.Health = 90;
+MonsterOne.Type = Element.Water;
+
+PlayerTwo.Monsters.Add(MonsterOne);
